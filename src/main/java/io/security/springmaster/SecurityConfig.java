e2a3326d453e40f1560f2c56b41eba73b5c3a0d5
ghdcksgml1 <ghdcksgml2@naver.com>
2025-02-24 21:50:33 +0900

    feat: formlogin

---
+++ src/main/java/io/security/springmaster/SecurityConfig.java
@@ -0,0 +1,53 @@
+package io.security.springmaster;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(httpSecurityHttpBasicConfigurer -> httpSecurityHttpBasicConfigurer.disable())
+                .formLogin(formLogin ->
+                        formLogin
+                                .loginPage("/loginPage")
+                                .loginProcessingUrl("/loginProc")
+                                .defaultSuccessUrl("/", true)
+                                .failureUrl("/failed")
+                                .usernameParameter("userId")
+                                .passwordParameter("passwd")
+                                .successHandler((request, response, authentication) -> {
+                                    System.out.println("authentication: " + authentication);
+                                    response.sendRedirect("/home");
+                                })
+                                .failureHandler((request, response, exception) -> {
+                                    System.out.println("exception: " + exception.getMessage());
+                                    response.sendRedirect("/login");
+                                })
+                                .permitAll()
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+                .password("{noop}1234")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}
---
+++ src/test/java/io/security/springmaster/SpringmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.springmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
